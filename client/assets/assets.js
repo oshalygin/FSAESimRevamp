5e4c938ab5df13544a8e29b966454b538c6480e4
Oleg Shalygin <oshalygin@gmail.com>
2016-07-23 00:56:51 -0700

    added assets subtree

---
+++ client/assets/assets.js
@@ -0,0 +1,36 @@
+// import "../../node_modules/jquery/dist/jquery.min";
+
+// import "../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css";
+// import "../../node_modules/font-awesome/css/font-awesome.min.css"
+// import "../../node_modules/toastr/build/toastr.min.css";
+// import "../../node_modules/flexslider/flexslider.css";
+// import "./css/styles.css";
+
+
+// import "../../node_modules/bootstrap/dist/js/bootstrap.min";
+// import "../../node_modules/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min";
+// import "./plugins/back-to-top";
+// import "../../node_modules/jquery-placeholder/jquery.placeholder";
+// // import "../../node_modules/fitvids/dist/fitvids.min";
+// import "../../node_modules/flexslider/jquery.flexslider-min";
+// import "./js/main";
+// import "./js/vimeo";
+
+
+// import "favicon.ico";
+
+// import "./plugins/bootstrap/css/bootstrap.min.css";
+// import "./plugins/font-awesome/css/font-awesome.css";
+// import "./plugins/flexslider/flexslider.css";
+// import "./css/styles.css";
+
+
+
+// import "./plugins/bootstrap/js/bootstrap.min.js";
+// import "./plugins/bootstrap-hover-dropdown.min.js";
+// import "./plugins/back-to-top.js";
+// import "./plugins/jquery-placeholder/jquery.placeholder.js";
+// import "./plugins/flexslider/jquery.flexslider-min.js";
+// import "./js/main.js";
+
+// import "http://a.vimeocdn.com/js/froogaloop2.min.js";
