d7e107eaded62273d3ca42680028aa362621188a
Oleg Shalygin <oshalygin@gmail.com>
2016-08-01 20:13:39 -0700

    porting over base code

--- server/application.js
+++ server/application.js
@@ -4,16 +4,16 @@ import path from "path";
 import bodyParser from "body-parser";
 import webpack from "webpack";
 import configuration from "../webpack.config.dev";
-import colors from "colors";
+import colors from "colors"; //eslint-disable-line no-unused-vars
 import open from "open";
 
-import router from "./routes";
+import router from "./routes"; //eslint-disable-line no-unused-vars
 
 let application = express();
 application.use(bodyParser.urlencoded({ extended: true }));
 application.use(bodyParser.json());
 
-let port = process.env.PORT || 8000;
+let port = process.env.PORT || 8000; //eslint-disable-line no-process-env
 
 const applicationCompiler = webpack(configuration);
 application.use(require("webpack-dev-middleware")(applicationCompiler, {
